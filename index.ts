e448f519338ec100beeecda5de1f86a29f863579
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-12-21 14:51:43 -0800

    fix passing initialState

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@
 
 interface Action {
   type: string;
-  payload?: any; //? mean optional
+  payload?: any; //? means optional
 }
 
 /* example
@@ -25,8 +25,8 @@ Store.value() - This is a getter, tells us what the state of our store is.
  */
 
 export class Store {
-  private state: { [key: string]: any };
   private reducers: { [key: string]: Function }
+  private state: { [key: string]: any };
 
   constructor(reducers = {}, initialState = {}) {
     this.reducers = reducers;
@@ -63,14 +63,7 @@ export const initialState = {
   loading: false
 };
 
-export function todosReducer(
-  state: {
-    data: [],
-    loaded: false,
-    loading: false
-  },
-  action: { type: string; payload: any }
-) {
+export function todosReducer(state = initialState, action: { type: string; payload: any }) {
   switch (action.type) {
     case 'ADD_TODO': {
       const todo = action.payload
@@ -91,7 +84,7 @@ const reducers = {
 const store = new Store(reducers);
 console.log("store", store.value);
 
-store.dispatch({
-  type: "ADD_TODO",
-  payload: { label: "eatPizza", complete: false }
-});
+// store.dispatch({
+//   type: "ADD_TODO",
+//   payload: { label: "eatPizza", complete: false }
+// });
